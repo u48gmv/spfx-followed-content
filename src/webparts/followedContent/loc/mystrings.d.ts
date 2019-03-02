77f5e6189b0f3aed65ffd15738e353def2e803dd
George Michael Mavridis <george-michael.mavridis@bg-phoenics.de>
2019-03-02 12:42:07 +0100

    First commit

---
+++ src/webparts/followedContent/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFollowedContentWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FollowedContentWebPartStrings' {
+  const strings: IFollowedContentWebPartStrings;
+  export = strings;
+}
