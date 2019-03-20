da0310de6ef051aa5207e8743339301ba65fb9ba
George Michael Mavridis <George-Michael.Mavridis@bg-phoenics.de>
2019-03-20 14:08:36 +0100

    Localisation added

---
+++ src/webparts/followedContent/loc/de-de.js
@@ -0,0 +1,9 @@
+define([], function() {
+    return {
+      "PropertyPaneDescription": "Konfiguration des WebParts",
+      "BasicGroupName": "Basis Elemente",
+      "TitleFieldLabel": "Titel",
+      "MySiteHostUrlLabel": "My Site Host URL"
+    }
+  });
+  
